b65c5da7c85dedea10c3a4cfa48314858cb20e44
Ishan Jain <ishanjain28@gmail.com>
2018-02-13 23:50:11 +0530

    Working on Communicating with Pulseaudio using Dbus

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate chrono;
+extern crate dbus;
 #[macro_use]
 extern crate lazy_static;
+extern crate nix;
 extern crate regex;
 
 mod pulseaudio;
@@ -11,6 +13,24 @@ fn main() {
 
     println!("{:?}", mods);
 
+    let id = pulseaudio::Modules::load(
+        "module-null-sink",
+        &[
+            ("sink_name", "ishan"),
+            ("sink_properties", "device.description=\"bt_speaker\""),
+        ],
+    );
+
+    println!("{:?}", id);
+
+    let mods = pulseaudio::Modules::get();
+
+    println!("{:?}", mods);
+
+    pulseaudio::Modules::unload(id.unwrap());
+    let mods = pulseaudio::Modules::get();
+
+    println!("{:?}", mods);
 
     println!("Hello world");
 }
--- src/pulseaudio.rs
+++ src/pulseaudio.rs
@@ -1,9 +1,7 @@
-extern crate lazy_static;
-extern crate regex;
-
 use std::process::{Command, ExitStatus};
-use std::{str, string};
+use std::{env, fs, path, str, string};
 use regex::Regex;
+use dbus;
 
 struct Pulseaudio {
     streams: Vec<String>,
@@ -14,6 +12,8 @@ struct Pulseaudio {
 
 pub struct Modules;
 
+const MODULE_NULL_SINK: &str = "module-null-sink";
+const MODULE_DBUS_PROTOCOL: &str = "module-dbus-protocol";
 
 impl Modules {
     pub fn get() -> Option<Vec<string::String>> {
@@ -21,13 +21,18 @@ impl Modules {
             static ref RE: Regex = Regex::new(r"(\d+)\s+([\w-]+)(.*?)\n").unwrap();
         }
 
-        let output = Command::new("pactl")
+        let output = match Command::new("pactl")
             .arg("list")
             .arg("modules")
             .arg("short")
             .output()
-            .expect("failed to execute \"pactl list modules short\"");
-
+        {
+            Ok(v) => v,
+            Err(e) => {
+                println!("{}", e);
+                return None;
+            }
+        };
 
         let s = str::from_utf8(&output.stdout).unwrap();
         let modules = RE.captures_iter(s)
@@ -38,14 +43,38 @@ impl Modules {
         Some(modules)
     }
 
-    fn load(mod_name: &str) {
-        Command::new("pactl").arg("load-module").arg(mod_name)
+    pub fn load(mod_name: &str, mods_param: &[(&str, &str)]) -> Option<u32> {
+        let mut args: Vec<String> = Vec::new();
+
+        for m in mods_param {
+            args.push(format!("{}={}", m.0, m.1));
+        }
+
+        let output = match Command::new("pactl")
+            .arg("load-module")
+            .arg(mod_name)
+            .args(args)
+            .output()
+        {
+            Ok(v) => v,
+            Err(e) => {
+                println!("{}", e);
+                return None;
+            }
+        };
+
+        let output = str::from_utf8(&output.stdout).unwrap().trim_right();
+
+        match output.parse::<u32>() {
+            Ok(v) => Some(v),
+            Err(e) => None,
+        }
     }
 
-    fn unload(module: &str) -> ExitStatus {
+    pub fn unload(module: u32) -> ExitStatus {
         Command::new("pactl")
             .arg("unload-module")
-            .arg(format!("{}", module))
+            .arg(module.to_string())
             .status()
             .expect(&format!(
                 "failed to unload module {} (\"pactl unload-module {}\")",
@@ -57,7 +86,7 @@ impl Modules {
 
 
 impl Pulseaudio {
-    fn new() -> Pulseaudio {
+    pub fn new() -> Pulseaudio {
         Pulseaudio {
             streams: vec![String::from("")],
             sinks: vec![String::from("")],
@@ -66,5 +95,112 @@ impl Pulseaudio {
         }
     }
 
-    fn connect(&mut self) {}
+    fn get_bus(&mut self) {
+        let mods = match Modules::get() {
+            Some(v) => v,
+            None => {
+                println!("error in fetching modules");
+                return;
+            }
+        };
+
+        match mods.into_iter().find(|x| x == MODULE_DBUS_PROTOCOL) {
+            Some(v) => {
+                println!("{} is already loaded", MODULE_DBUS_PROTOCOL);
+            }
+            None => match Modules::load(MODULE_DBUS_PROTOCOL, &[]) {
+                Some(v) => println!("loaded {}({})", MODULE_DBUS_PROTOCOL, v),
+                None => {
+                    println!("failed to load {}", MODULE_DBUS_PROTOCOL);
+                }
+            },
+        }
+
+        //TODO: Incomplete
+    }
+
+    fn get_bus_addresses(&mut self) {
+        let mut bus_addresses: Vec<String> = Vec::new();
+
+        // Probe PULSE_DBUS_SERVER
+        match env::var("PULSE_DBUS_SERVER") {
+            Ok(v) => for addr in v.split(";") {
+                bus_addresses.push(addr.to_owned());
+            },
+            Err(e) => {
+                println!("failed to probe $PULSE_DBUS_SERVER {}", e);
+            }
+        };
+
+
+        // Probe /run/pulse/dbus-socket
+        match fs::File::open("/run/pulse/dbus-socket") {
+            Ok(v) => {
+                bus_addresses.push("unix:path=/run/pulse/dbus-socket".to_owned());
+            }
+            Err(e) => {
+                println!("error in probing /run/pulse/dbus-socket {}", e);
+            }
+        }
+
+        // Probe XDG_RUNTIME_DIR
+        match env::var("XDG_RUNTIME_DIR") {
+            Ok(v) => {
+                let p = path::Path::new(&v).join("pulse/dbus-socket");
+
+                match fs::File::open(&p) {
+                    Ok(v) => {
+                        bus_addresses.push(format!("unix:path={}", p.to_str().unwrap()));
+                    }
+                    Err(e) => {
+                        println!("error in probing $XDG_RUNTIME_DIR {}", e);
+                    }
+                };
+            }
+            Err(e) => {
+                println!("failed to probe $XDG_RUNTIME_DIR {}", e);
+            }
+        }
+
+        // dbus_server_lookup
+        let addr = self.dbus_server_lookup();
+        match addr {
+            Some(v) => {}
+            None => {
+                println!("failed in dbus server lookup");
+            }
+        };
+    }
+
+    fn dbus_server_lookup(&mut self) -> Option<String> {
+        let conn = dbus::Connection::get_private(dbus::BusType::Session).unwrap();
+
+
+        return Some("ishan".to_owned());
+    }
+
+    pub fn connect(&mut self, signals: &str) {}
+
+    pub fn update_sinks(&mut self) {}
+
+    pub fn create_null_sink(&mut self, name: &str, desc: &str) {
+        let mod_id = Modules::load(
+            MODULE_NULL_SINK,
+            &[
+                ("sink_name", name),
+                (
+                    "sink_properties",
+                    &format!("device.description={}", desc.replace(" ", "\\ ")),
+                ),
+            ],
+        );
+
+        if mod_id.unwrap() > 0 {
+            self.update_sinks();
+        }
+    }
+
+    pub fn delete_null_sink(mod_id: u32) -> ExitStatus {
+        Modules::unload(mod_id)
+    }
 }
